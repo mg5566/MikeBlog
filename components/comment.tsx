9d081ce1bacb571d22b9076700a4ab109d7b85f4
Mike <mike.kangaroo.world@gmail.com>
2025-03-10 19:29:01 +0900

    [feat] add comments

--- components/comment.tsx
+++ components/comment.tsx
@@ -2,25 +2,26 @@
 // 1. Uncomment this component
 // 2. Update repo, repoId, and categoryId with your own giscus settings
 // 3. Visit https://giscus.app to get your settings
+"use client";
 
-// import Giscus from "@giscus/react";
+import Giscus from "@giscus/react";
 
-// export default function Comment() {
-//   return (
-//     <Giscus
-//       id="comments"
-//       repo="https://github.com/mg5566/MikeBlog"
-//       repoId=""
-//       category="Announcements"
-//       categoryId=""
-//       mapping="pathname"
-//       strict="0"
-//       reactionsEnabled="1"
-//       emitMetadata="0"
-//       inputPosition="bottom"
-//       theme="preferred_color_scheme"
-//       lang="ko"
-//       loading="lazy"
-//     />
-//   );
-// }
+export default function Comment() {
+  return (
+    <Giscus
+      id="comments"
+      repo="mg5566/MikeBlog"
+      repoId="R_kgDOOF8DRQ"
+      category="General"
+      categoryId="DIC_kwDOOF8DRc4CnxYR"
+      mapping="pathname"
+      strict="0"
+      reactionsEnabled="1"
+      emitMetadata="0"
+      inputPosition="bottom"
+      theme="preferred_color_scheme"
+      lang="ko"
+      loading="lazy"
+    />
+  );
+}
